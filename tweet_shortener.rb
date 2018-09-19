226d52fdce313d17edf0ebf6e2a6faed4f91f80d
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 20:57:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,7 +16,7 @@ end
 
 def word_substituter(tweet)
   tweet.split("").collect do |word|
-    if word 
+    if dictionary.keys.include?(word.downcase)
       word = dictionary.keys
     end 
   end 
