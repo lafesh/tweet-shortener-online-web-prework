afa4ccda6496ffe850fbea2386e628300e4e5204
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 20:54:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(tweet)
   tweet.split("").collect do |word|
     if word && dictionary.keys
-      word = dictionary[keys]
+      word = dictionary.keys
     end 
   end 
   
