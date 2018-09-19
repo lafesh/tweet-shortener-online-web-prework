e3c85ddb52f295f3caa31a2fcc09465d2a4fa7f6
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 21:10:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,3 +29,11 @@ def bulk_tweet_shortener(tweet)
     puts word_substituter(word)
   end 
 end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 139
+    puts bulk_tweet_shortener
+  else
+    tweet
+  end 
+end 
